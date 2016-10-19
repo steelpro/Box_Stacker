66c75ba6d50bdd1c39946a5442f988e44a6f5130
Zachary Betters <zachbet97@outlook.com>
2016-10-19 08:24:23 -0400

    Add project files.

---
+++ Exam2/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace Exam2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEmpty = new System.Windows.Forms.Button();
+            this.btnShip = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pbxRed = new System.Windows.Forms.PictureBox();
+            this.pbxGreen = new System.Windows.Forms.PictureBox();
+            this.pbxBlue = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxRed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxGreen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxBlue)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnEmpty
+            // 
+            this.btnEmpty.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnEmpty.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEmpty.Location = new System.Drawing.Point(12, 286);
+            this.btnEmpty.Name = "btnEmpty";
+            this.btnEmpty.Size = new System.Drawing.Size(80, 51);
+            this.btnEmpty.TabIndex = 0;
+            this.btnEmpty.Text = "Empty Box";
+            this.btnEmpty.UseVisualStyleBackColor = true;
+            this.btnEmpty.Click += new System.EventHandler(this.btnEmpty_Click);
+            // 
+            // btnShip
+            // 
+            this.btnShip.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnShip.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnShip.Location = new System.Drawing.Point(134, 286);
+            this.btnShip.Name = "btnShip";
+            this.btnShip.Size = new System.Drawing.Size(79, 51);
+            this.btnShip.TabIndex = 1;
+            this.btnShip.Text = "Ship Box";
+            this.btnShip.UseVisualStyleBackColor = true;
+            this.btnShip.Click += new System.EventHandler(this.btnShip_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(251, 286);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(80, 51);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Wheat;
+            this.pictureBox1.Location = new System.Drawing.Point(125, 241);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 20);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Wheat;
+            this.pictureBox2.Location = new System.Drawing.Point(125, 224);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 20);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Wheat;
+            this.pictureBox3.Location = new System.Drawing.Point(125, 205);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 20);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Wheat;
+            this.pictureBox4.Location = new System.Drawing.Point(125, 187);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(100, 20);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.Wheat;
+            this.pictureBox5.Location = new System.Drawing.Point(125, 170);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(100, 20);
+            this.pictureBox5.TabIndex = 3;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.Wheat;
+            this.pictureBox6.Location = new System.Drawing.Point(125, 151);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(100, 20);
+            this.pictureBox6.TabIndex = 3;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BackColor = System.Drawing.Color.Wheat;
+            this.pictureBox7.Location = new System.Drawing.Point(125, 133);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(100, 20);
+            this.pictureBox7.TabIndex = 3;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.BackColor = System.Drawing.Color.Wheat;
+            this.pictureBox8.Location = new System.Drawing.Point(125, 114);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(100, 20);
+            this.pictureBox8.TabIndex = 3;
+            this.pictureBox8.TabStop = false;
+            // 
+            // pbxRed
+            // 
+            this.pbxRed.BackColor = System.Drawing.Color.Red;
+            this.pbxRed.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbxRed.Location = new System.Drawing.Point(12, 58);
+            this.pbxRed.Name = "pbxRed";
+            this.pbxRed.Size = new System.Drawing.Size(100, 20);
+            this.pbxRed.TabIndex = 4;
+            this.pbxRed.TabStop = false;
+            this.pbxRed.Click += new System.EventHandler(this.pbxRed_Click);
+            // 
+            // pbxGreen
+            // 
+            this.pbxGreen.BackColor = System.Drawing.Color.Green;
+            this.pbxGreen.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbxGreen.Location = new System.Drawing.Point(125, 38);
+            this.pbxGreen.Name = "pbxGreen";
+            this.pbxGreen.Size = new System.Drawing.Size(100, 40);
+            this.pbxGreen.TabIndex = 5;
+            this.pbxGreen.TabStop = false;
+            this.pbxGreen.Click += new System.EventHandler(this.pbxGreen_Click);
+            // 
+            // pbxBlue
+            // 
+            this.pbxBlue.BackColor = System.Drawing.Color.Blue;
+            this.pbxBlue.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbxBlue.Location = new System.Drawing.Point(242, 18);
+            this.pbxBlue.Name = "pbxBlue";
+            this.pbxBlue.Size = new System.Drawing.Size(89, 60);
+            this.pbxBlue.TabIndex = 6;
+            this.pbxBlue.TabStop = false;
+            this.pbxBlue.Click += new System.EventHandler(this.pbxBlue_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(346, 349);
+            this.ControlBox = false;
+            this.Controls.Add(this.pbxBlue);
+            this.Controls.Add(this.pbxGreen);
+            this.Controls.Add(this.pbxRed);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnShip);
+            this.Controls.Add(this.btnEmpty);
+            this.Name = "Form1";
+            this.Text = "Jimmy John\'s Box Company";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxRed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxGreen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxBlue)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEmpty;
+        private System.Windows.Forms.Button btnShip;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.PictureBox pbxRed;
+        private System.Windows.Forms.PictureBox pbxGreen;
+        private System.Windows.Forms.PictureBox pbxBlue;
+    }
+}
+
---
+++ Exam2/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Windows.Forms;
+
+/*
+ * Zachary Betters
+ * CIS 209
+ * October 12, 2016
+ * This program will add boxes to a crate and ship them or clear them 
+ */
+
+namespace Exam2 {
+    public partial class Form1 : Form {
+        
+        //array created to hold boxes
+        PictureBox[] boxes;
+
+        public Form1() {
+            InitializeComponent();
+            
+            //all picture boxes added to array
+            boxes = new PictureBox[] {pictureBox1, pictureBox2, pictureBox3,
+            pictureBox4, pictureBox5, pictureBox6, pictureBox7, pictureBox8 };     
+        }   
+        
+        private void pbxRed_Click(object sender, EventArgs e) {
+
+            //red box size of 1, so if first empty slot is 1 or more
+            if (isRoomAvailable(1)) {
+            
+                //first empty slot changes to red
+                boxes[FirstEmptySlot()].BackColor = System.Drawing.Color.Red;
+            }
+            //if there are no empty slots left
+            else {
+                MessageBox.Show("Cannot add anymore red boxes! Please ship or empty!",
+                "Error!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }              
+        }
+        private void pbxGreen_Click(object sender, EventArgs e) {
+            if (isRoomAvailable(2)) {
+                boxes[FirstEmptySlot()].BackColor = System.Drawing.Color.Green;
+                boxes[FirstEmptySlot()].BackColor = System.Drawing.Color.Green;
+            }
+            else {
+                MessageBox.Show("Cannot add anymore green boxes! Please ship or empty!",
+                "Error!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        private void pbxBlue_Click(object sender, EventArgs e) {
+            if (isRoomAvailable(3)) {
+                boxes[FirstEmptySlot()].BackColor = System.Drawing.Color.Blue;
+                boxes[FirstEmptySlot()].BackColor = System.Drawing.Color.Blue;
+                boxes[FirstEmptySlot()].BackColor = System.Drawing.Color.Blue;
+            }
+            else {
+                MessageBox.Show("Cannot add anymore blue boxes! Please ship or empty!",
+                "Error!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }     
+           
+        private void btnEmpty_Click(object sender, EventArgs e) { 
+            if (isEmpty()) 
+                MessageBox.Show("Please fill box before emptying!",
+                "Error!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else EmptyBox();
+        }
+        private void btnShip_Click(object sender, EventArgs e) {
+            if (isEmpty()) 
+                MessageBox.Show("Please fill box before shipping!",
+                "Error!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else EmptyBox();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e) { Close(); }  
+
+        public int FirstEmptySlot() {
+            //i is set to 10 which is outside the number of pictureboxes 
+            int i = 10;
+            for (int a = 0; a < boxes.Length; a++) {
+                
+            //if a box has the color of wheat...              
+                if (boxes[a].BackColor == System.Drawing.Color.Wheat) { 
+                    //...i changes from 10 to that number box and the loop stops
+                    i = a; break; }
+            } 
+            return i;
+        } 
+
+        public bool isRoomAvailable(int n) {
+            //if the first empty slot plus the size of the..
+            //...box selected does not exceed 8
+            if (FirstEmptySlot() + n <= 8) return true; 
+            else return false;
+        }
+
+        public bool isEmpty() {
+            //if the first empty slot is less than 1, or if no boxes are added
+            if (FirstEmptySlot() >= 1) return false;
+            else return true;
+        }
+                     
+        void EmptyBox() {
+            for (int i = 0; i < boxes.Length; i++) {
+                //each picturebox in the array is changed back to wheat when called
+                boxes[i].BackColor = System.Drawing.Color.Wheat; }
+        }  
+    }
+}
